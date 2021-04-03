c287e7e51214225a8d04dae77dee5b5599212abf
xavierbrasher <xavier.brasher@gmail.com>
2021-04-03 21:09:17 +0800

    main

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let audio = new Audio("lets-go.mp3");
+
+function sleep(seconds) {
+  let milliseconds = seconds * 1000;
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+function switchImage() {
+  document.getElementById("letsGo").innerHTML = '<img src="/img/before.png" />';
+}
+
+function daBaby() {
+  audio.play();
+  sleep(2.2);
+  document.getElementById("letsGo").innerHTML = '<img src="img/after1.png" />';
+  sleep(2);
+  document.getElementById("letsGo").innerHTML = '<img src="img/after.png" />';
+  sleep(1);
+  document.getElementById("letsGo").innerHTML = '<img src="/img/before.png" />';
+}
